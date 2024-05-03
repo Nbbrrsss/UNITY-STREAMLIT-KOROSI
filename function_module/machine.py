aff1793e8f9a6a71db7c8432b3f950d575adf34b
Nibras Ardyansyah <115968616+Nbbrrsss@users.noreply.github.com>
2024-05-03 16:19:08 +0700

    Update machine.py

--- function_module/machine.py
+++ function_module/machine.py
@@ -274,7 +274,7 @@ def base_machine_learning():
                 # Read the uploaded CSV file
                 df_uploaded = pd.read_csv(uploaded_file)
                 df_concat = df_uploaded.copy()
-                df_concat.drop('IE EXP%' , axis = 1, inplace = True)
+                df_concat.drop('IE EXP (%)' , axis = 1, inplace = True)
 
                 # Define columns to scale
                 columns_to_scale = ['Molecular_weight MW (g/mol)', 'pKa', 'Log P', 'Log S', 'Polar Surface Area (Å2)',
