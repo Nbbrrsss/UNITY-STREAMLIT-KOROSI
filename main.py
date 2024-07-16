a3b069104f5779e1001a5f7f02c552931113a02f
Nibras Ardyansyah <nibrasbahy@gmail.com>
2024-07-16 20:38:37 +0700

    Update main.py
    english

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ from function_module.chatbot import chatbot
 # ============== End of Import Library ============
 
 st.set_page_config(
-    page_title="Website Inhibitor Korosi",
+    page_title="Corrosion Inhibitor Website",
     page_icon="labmatics icon.ico"
 )
 
@@ -41,29 +41,29 @@ data_unduh = dth.data_unduh()
 with st.sidebar:
     st.image('labmatics.png', width=350)
     menuweb = st.radio(
-        "Menu Website", ["Home", "ChatBot", "Efisiensi Anti Korosi","Analisis Data"])
+        "Website Menu", ["Home", "ChatBot", "Anti-corrosion Efficiency","Data Analysis"])
 
 st.image("Group 10.png", width=350)
 if menuweb == "Home":
-    st.title("Senyawa Obat Inhibitor Korosi")
-    st.markdown("Senyawa inhibitor korosi adalah zat atau senyawa kimia yang ditambahkan ke lingkungan yang berpotensi mengakibatkan korosi untuk mencegah atau mengurangi laju korosi logam atau material lain. Inhibitor korosi bertujuan untuk melindungi permukaan logam dari oksidasi atau reaksi kimia lain yang dapat mengurangi kekuatannya atau mempersingkat umur pemakaiannya.")
+    st.title("Corrosion Inhibitor Drugs Compounds")
+    st.markdown("Corrosion inhibitor compounds are substances or chemical compounds that are added to a potentially corrosive environment to prevent or reduce the rate of corrosion of metals or other materials. Corrosion inhibitors aim to protect metal surfaces from oxidation or other chemical reactions that can reduce their strength or shorten their service life.")
 
     tab1, tab2, tab3 = st.tabs(
-        ['Kamus Senyawa', 'Filter Senyawa', "Unduh Dataset"])
+        ['Compound Dictionary', 'Compound Filter', "Download Dataset"])
 
     with tab1:
 
-        st.header("Kamus Senyawa")
+        st.header("Compound Dictionary")
         kolom_filter = {
-            'Common_name': 'Nama Senyawa',
-            'Formula': 'Rumus Senyawa',
-            'IUPAC_name': 'Tata nama senyawa kimia',
+            'Common_name': 'Compound Name',
+            'Formula': 'Compound Formula',
+            'IUPAC_name': 'Chemical compound name system',
         }
 
-        to_filter_columns = st.selectbox("Masukkan filter Senyawa untuk ditampilkan", options=list(
+        to_filter_columns = st.selectbox("Input Compound Filter to display", options=list(
             kolom_filter.keys()), format_func=lambda x: kolom_filter[x])
 
-        text_search = st.text_input("Cari informasi tentang senyawa", value="")
+        text_search = st.text_input("Search for information about compounds", value="")
         text_search = text_search.lower()
 
         df["Common_name"] = df["Common_name"].str.lower()
@@ -89,10 +89,10 @@ if menuweb == "Home":
                     st.markdown(
                         f"[**{row['Common_name'].strip().capitalize()}**]({row['Ref']})")
                     st.markdown(
-                        f"{row['Common_name'].capitalize()} adalah sebuah senyawa dengan Rumus Kimia **{row['Formula'].upper()}**.Senyawa dengan Tata Nama Kimia **{row['IUPAC_name']}** ini memiliki Persentase Energi Ionisasi sebesar **{round(row['IE EXP (%)'],2)}%**")
-                    st.markdown(f"Berat molekul dari senyawa ini adalah {row['Molecular_weight MW (g/mol)']} g/mol. {row['Common_name'].capitalize()} memiliki kemampuan untuk membentuk ikatan hidrogen, dengan Hydrogen Acceptor Count sebanyak {row['Hydrogen Acceptor Count']} dan Hydrogen Donor Count sebanyak {row['Hydrogen Donor Count']}. Selain itu, kemampuan atom dalam molekul ini untuk menarik pasangan elektron sebesar {row['Electronegativity (eV)']} dan ukuran kestabilan molekulnya juga mencapai {row['Electronegativity (eV)']}.")
+                        f"{row['Common_name'].capitalize()} is a compound with Chemical Formula **{row['Formula'].upper()}**.Compounds with Chemical Names **{row['IUPAC_name']}** it has an Ionization Energy Percentage of **{round(row['IE EXP (%)'],2)}%**")
+                    st.markdown(f"The molecular weight of this compound is {row['Molecular_weight MW (g/mol)']} g/mol. {row['Common_name'].capitalize()} has the ability to form hydrogen bonds, with Hydrogen Acceptor Count as much as {row['Hydrogen Acceptor Count']} and Hydrogen Donor Count as much as {row['Hydrogen Donor Count']}. In addition, the ability of the atoms in this molecule to attract electron pairs as large as {row['Electronegativity (eV)']} and the size of the molecular stability also reaches {row['Electronegativity (eV)']}.")
             else:
-                st.write("Senyawa tidak ditemukan!")
+                st.write("Compound not found!")
         else:
             N_cards_per_row = 3
             for n_row, row in df.reset_index().iterrows():
@@ -108,30 +108,30 @@ if menuweb == "Home":
                         f"**{row['Formula'].upper()}**")
                     st.markdown(f"**{row['IUPAC_name']}**")
                     st.markdown(
-                        f"Persentase Energi Ionisasi **{round(row['IE EXP (%)'],2)}%**")
+                        f"Ionization Energy Percentage **{round(row['IE EXP (%)'],2)}%**")
     with tab2:
-        st.error('Perhatian! Data IE EXP (Persentase eksperimental dari efisiensi penghambatan korosi) yang kami sajikan merupakan hasil dari gabungan prediksi dari model Machine Learning yang telah kami buat.Jika ingin melihat dataset asli bisa download pada Tab "Unduh Dataset" ')
+        st.error('Attention! The IE EXP (Experimental percentage of corrosion inhibition efficiency) data we present is the result of combined predictions from the Machine Learning models we have created. If you want to see the original dataset, you can download it from the “Download Dataset” tab." ')
 
-        st.header("Filter Senyawa")
+        st.header("Compound Filter")
 
         kolom_filter = {
-            'Molecular_weight MW (g/mol)': 'Berat Molekul (g/mol)',
-            'pKa': 'Kekuatan Asam',
-            'Log P': 'Kelarutan Lemak',
-            'Log S': 'Kelarutan Dalam Air',
-            'Polar Surface Area': 'Luas permukaan molekul',
-            'Polarizability': 'Polarisabilitas',
-            'HOMO (eV)': 'Energi orbital molekul yang terisi',
-            'LUMO (eV)': 'Energi orbital molekul yang tidak terisi',
+            'Molecular_weight MW (g/mol)': 'Molecular Weight (g/mol)',
+            'pKa': 'Acid Strength',
+            'Log P': 'Fat Solubility',
+            'Log S': 'Solubility in Water',
+            'Polar Surface Area': 'Molecular surface area',
+            'Polarizability': 'Polarizability',
+            'HOMO (eV)': 'Energy of filled molecular orbitals',
+            'LUMO (eV)': 'Unfilled molecular orbital energy',
             'Electronegativity (eV)': 'Electronegativity (eV)',
-            ' ?N_Fe ': 'Indeks daya tunduk molekul',
-            'IE EXP (%)': 'Efisiensi Anti Korosi',
+            ' ?N_Fe ': 'Index of the molecule\'s resistivity',
+            'IE EXP (%)': 'Anti-corrosion efficiency',
         }
 
-        # add filter senyawa by kategori
+        # add Compound Filter by kategori
         kolom_filtertab2 = {
-            'all': 'Semua',
-            'Drugs': 'Senyawa Drugs'
+            'all': 'All',
+            'Drugs': 'Compound Drugs'
         }
 
         # default kolom all
@@ -140,7 +140,7 @@ if menuweb == "Home":
         col_filter3, col_filter4 = st.columns(2)
 
         def filter_dataframe(df: pd.DataFrame) -> pd.DataFrame:
-            to_filter_columns = st.multiselect("Masukkan filter Senyawa untuk ditampilkan", options=list(
+            to_filter_columns = st.multiselect("Insert Compound Filter to display", options=list(
                 kolom_filter.keys()), format_func=lambda x: kolom_filter[x])
 
             for column in to_filter_columns:
@@ -151,7 +151,7 @@ if menuweb == "Home":
                 _max = float(df[column].max())
                 step = (_max - _min) / 100
                 user_num_input = right.slider(
-                    f"Ukuran {column} senyawa",
+                    f"Size of {column} compound",
                     _min,
                     _max,
                     (_min, _max),
@@ -186,18 +186,18 @@ if menuweb == "ChatBot":
     chatbot()
 
 # ========================= Halaman App ========================================================================================================================================================================================================================================================================================================================================================================
-if menuweb == "Efisiensi Anti Korosi":
+if menuweb == "Anti-corrosion Efficiency":
     # Streamlit App Header
-    st.title("Prediksi IE% Korosi dalam senyawa")
+    st.title("Predicted IE% Corrosion in compounds")
     st.markdown(
-        "Silakan prediksi Efisiensi Korosi Senyawa menggunakan parameter-parameter yang ada!")
-    st.error('Berdasarkan eksperimen, Model Terbaik lebih disarankan untuk memprediksi Senyawa dengan baik karena memiliki nilai akurasi yang tinggi dan nilai error yang rendah. Kombinasi model yang lain sebaiknya hanya digunakan sebagai referensi atau mungkin hanya menjadi pembanding.')
+        "Please predict the Corrosion Efficiency of the Compound using the parameters!")
+    st.error('Based on experiments, the Best Model is more recommended to predict the Compound well because it has a high accuracy value and a low error value. Other model combinations should only be used as a reference or perhaps just a comparison.')
     opsi_model = st.selectbox(
-        "", ['Experiment Machine Learning','Analisis Model Terbaik'])
+        "", ['Experimental Machine Learning','Analysis of the Best Model'])
 
     st.write("---")
 
-    if opsi_model == "Experiment Machine Learning":
+    if opsi_model == "Experimental Machine Learning":
         base_machine_learning()
     else:
         analisis_model_terbaik()
@@ -206,6 +206,6 @@ if menuweb == "Efisiensi Anti Korosi":
 # ========================= Ahkir halaman App ===================================================================================================================================================================================================================================================================================================================================================================
 
 # Data Analisis
-if menuweb == "Analisis Data":
-    st.title("Data Set")
+if menuweb == "Data Analysis":
+    st.title("Dataset")
     analisis()
